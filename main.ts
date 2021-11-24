248fe08b2314a34f8ca550af9a3e5f208a029952
omarturmun <93262369+omarturmun@users.noreply.github.com>
2021-11-24 14:12:57 +0100

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,25 @@
+input.onButtonPressed(Button.A, function () {
+    jokalaria.move(-1)
+})
+input.onButtonPressed(Button.B, function () {
+    jokalaria.move(1)
+})
+let meteoritoa: game.LedSprite = null
+let jokalaria: game.LedSprite = null
+jokalaria = game.createSprite(2, 4)
+basic.forever(function () {
+    basic.pause(randint(1000, 1500))
+    meteoritoa = game.createSprite(randint(0, 4), 0)
+    for (let index = 0; index < 4; index++) {
+        basic.pause(200)
+        meteoritoa.change(LedSpriteProperty.Y, 1)
+    }
+    if (meteoritoa.isTouching(jokalaria)) {
+        game.gameOver()
+    }
+    basic.pause(200)
+    if (meteoritoa.isTouchingEdge()) {
+        meteoritoa.delete()
+        game.setScore(1)
+    }
+})
